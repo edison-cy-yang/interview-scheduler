f7dd51385b8b4be91cc5606474b3a3232641b369
edison-cy-yang <edison.cy.yang@gmail.com>
2020-01-20 15:32:56 -0800

    added Appointment component and its default storybook behaviour

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
 
 storiesOf("Button", module)
   .addParameters({
@@ -127,4 +128,15 @@ storiesOf("InterviewerListItem", module)
         value={3}
         onChange={action("setInterviewer")}
       />
+    ));
+
+  storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("12PM", () => (
+    <Appointment
+      time="12PM"
+    />
     ));
