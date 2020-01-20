b9fbecd1ddd54ea684c7fd1741c3d8d01cc0c898
edison-cy-yang <edison.cy.yang@gmail.com>
2020-01-20 15:11:08 -0800

    InterviewersListItem and InterviewersList components and code for storybook

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import InterviewerListItem from "components/InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const interviewerListItems = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        selected={interviewer.id === props.value}
+        avatar={interviewer.avatar}
+        setInterviewer={() => props.onChange(interviewer.id)}
+      >
+      </InterviewerListItem>
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerListItems}
+      </ul>
+    </section>
+  )
+};
