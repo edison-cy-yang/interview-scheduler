83fc9d07731ed6ae3c85e9b27710469643d36530
edison-cy-yang <edison.cy.yang@gmail.com>
2020-01-22 13:43:33 -0800

    render different component based on appointment state, use custom hook useVisualMode to transition to different mode, which Appointment uses to conditionally render the components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import "./styles.scss";
 
@@ -6,27 +6,35 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
-  if (props.interview) {
-    return (
-      <article className="appointment">
-        <Header 
-          time={props.time}
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => {transition(CREATE)}} />}
+      {mode === SHOW && (
+        <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={props.onSave}
+          onCancel={() => {back()}}
         />
-        <Show 
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-        />
-      </article>
-    )
-  } else {
-    return (
-      <article className="appointment">
-        <Header 
-          time={props.time}
-        />
-        <Empty />
-      </article>
-    )
-  }
-};
+      )}      
+    </article>
+  );
+}
