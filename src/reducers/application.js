6f957545c9f2940c3534607bf76a0b11c2af82d1
edison-cy-yang <edison.cy.yang@gmail.com>
2020-01-28 14:51:18 -0800

    moved reducer to separate file, tested the reducer will generate error when given unsupported type

---
+++ src/reducers/application.js
@@ -0,0 +1,87 @@
+export const SET_DAY = "SET_DAY";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+export const SET_INTERVIEW = "SET_INTERVIEW";
+export const DELETE_INTERVIEW = "DELETE_INTERVIEW";
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return { ...state, day: action.value };
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        days: action.value.days,
+        appointments: action.value.appointments,
+        interviewers: action.value.interviewers
+      };
+    case SET_INTERVIEW: {
+      //copy the specific appointment object with id, as well as the interview object
+      const appointment = {
+        ...state.appointments[action.value.id],
+        interview: { ...action.value.interview }
+      };
+      //Now we have a single appointment object, update this appointment in the appointments object
+      const appointments = {
+        ...state.appointments,
+        [action.value.id]: appointment
+      };
+
+      //if interview is null, then we are creating a new interview, update spot count
+      //if not null, we are editing existing interview, dont update spot count
+      if (state.appointments[action.value.id].interview) {
+        return { ...state, appointments};
+      } else {
+        const days = updateCount(state.days, {
+          type: SET_INTERVIEW,
+          day: state.day
+        });
+        return { ...state, appointments, days };
+      }
+    }
+    case DELETE_INTERVIEW: {
+      //set appointment interview to null
+      const appointment = {
+        ...state.appointments[action.value.id],
+        interview: null
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [action.value.id]: appointment
+      };
+
+      const days = updateCount(state.days, {
+        type: DELETE_INTERVIEW,
+        day: state.day
+      });
+
+      return { ...state, appointments, days };
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+function updateCount(array, action) {
+  return array.map((item, index) => {
+    if (item.name !== action.day) {
+      return item;
+    } else {
+      if (action.type === SET_INTERVIEW) {
+        return {
+          ...item,
+          spots: item.spots - 1
+        };
+      } else if (action.type === DELETE_INTERVIEW) {
+        return {
+          ...item,
+          spots: item.spots + 1
+        };
+      } else {
+        return item;
+      }
+    }
+  });
+}
---
+++ src/reducers/application.test.js
@@ -0,0 +1,9 @@
+import reducer from './application';
+
+describe("reducer", () => {
+  it("thows an error with an unsupported type", () => {
+    expect(() => reducer({}, { type: null })).toThrowError(
+      /tried to reduce with unsupported action type/i
+    );
+  });
+})
